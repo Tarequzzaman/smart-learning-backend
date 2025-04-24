63572862f3304e0139dd6bbc3d1b32e156c417b0
Tarequzzaman <tareqcse12@gmail.com>
2025-04-24 20:14:43 +1000

    # Add: - sign up API created - log in API created - demo-create topic api created

--- README.md
+++ README.md
@@ -28,3 +28,27 @@ docker-compose up   -d
 
 Visit: 
 http://127.0.0.1:8004/docs
+
+Log in API: 
+
+```
+curl -X 'POST' \
+  'http://0.0.0.0:8004/log_in' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'grant_type=password&username=tareqcse12%40gmail.com&password=admin123&scope=&client_id=&client_secret='
+
+```
+
+Create Topic API
+
+```
+curl --location 'http://0.0.0.0:8004/topics' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0YXJlcWNzZTEyQGdtYWlsLmNvbSIsImV4cCI6MTc0NTQ5MjExOX0.QfkL7NQkzG-gL7KupkYLzptefRBsy9iFTk0XiAmzuzg' \
+--data '{
+  "title": "string",
+  "description": "string",
+  "created_by": 0
+}'
+```
--- app/db/schemas.py
+++ app/db/schemas.py
@@ -1,16 +1,43 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, EmailStr
+from typing import Union
 
-class UserBase(BaseModel):
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    first_name: str
+    last_name: str
+    role: str
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+class TokenWithUser(BaseModel):
+    access_token: str
+    token_type: str
+    user: dict  # or
+
+class LogInUser(BaseModel):
     email: str
-
-
-class UserCreate(UserBase):
     password: str
 
 
-class User(UserBase):
-    id: int
-    is_active: bool
-    class Config:
-        orm_mode = True
+
+class TopicCreate(BaseModel):
+    title: str
+    description: str
+    created_by: int
+
+
+class TokenData(BaseModel):
+    email: Union[str, None] = None
